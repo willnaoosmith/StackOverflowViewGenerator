a95d5eda405488b18803a346e9e0b9ac8ffa033c
William Brochensque Júnior <55159911+willnaoosmith@users.noreply.github.com>
2023-11-09 08:52:04 -0300

    Made it work with newer selenium

--- StackOverflowAutomaticViews.py
+++ StackOverflowAutomaticViews.py
@@ -1,6 +1,9 @@
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-import time, requests, json, sys
+from selenium.webdriver.common.by import By
 from selenium import webdriver
+import requests, json, sys, os
+from time import sleep
 
 UserLoginList = [json.loads(credential) for credential in sys.argv[1].split('|')]
 UserID = sys.argv[2]
@@ -12,24 +15,24 @@ LinkList = [question['link'] for question in search['items'] if question['view_c
 def generateViews(browser):
 	for link in LinkList:
 		browser.get(link)
-		time.sleep(2)
+		sleep(2)
 
 def Login(browser, user, password):
 	browser.get(r'https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f')
-	time.sleep(2)
+	sleep(2)
 
-	browser.find_element_by_id('email').send_keys(user)
-	browser.find_element_by_name('password').send_keys(password)
-	time.sleep(1)
+	browser.find_element(By.ID, 'email').send_keys(user)
+	browser.find_element(By.NAME, 'password').send_keys(password)
+	sleep(1)
 
-	browser.find_element_by_name('submit-button').click()
-	time.sleep(2)
+	browser.find_element(By.NAME, 'submit-button').click()
+	sleep(2)
 
 try:
-
 	options = Options()
 	options.add_argument("--headless")
-	browser = webdriver.Firefox(options=options, executable_path=r'./geckodriver')
+	service = Service(log_path=os.devnull)
+	browser = webdriver.Firefox(options=options, service=service)
 	
 	generateViews(browser)
 
@@ -40,7 +43,7 @@ try:
 		generateViews(browser)
 
 		browser.get("https://stackoverflow.com/users/logout")
-		browser.find_element_by_xpath("//*[@class='grid--cell s-btn s-btn__primary']").click()
+		browser.find_element(By.XPATH, "//*[@class='flex--item s-btn s-btn__filled']").click()
 
 except Exception as error:
 	print(error)
